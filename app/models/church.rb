4d56ff8c58ff0772de5cab6986361c3b4ae17fb6
mikelangston <mikemac8203@gmail.com>
2016-01-24 09:58:51 -0500

    added test for church and updated carousel

--- app/models/church.rb
+++ app/models/church.rb
@@ -1,3 +1,5 @@
 class Church < ActiveRecord::Base
   belongs_to :user
+
+  validates :name, :denomination, presence: true
 end
--- spec/models/church_spec.rb
+++ spec/models/church_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Church, type: :model do
+  let (:church) { Church.new }
 
+  context "New Church without name or denomination" do
+    it "should not be valid" do
+      expect(church.valid?).to be_falsey
+    end
+
+    it "should not save" do
+      expect(church.save).to be_falsey
+    end
+  end
+
+  context "New Church with name and denomination" do
+    before do
+      church.name = "SCCC"
+      church.denomination = "Non-Denominational"
+    end
+
+    it "should be valid" do
+      expect(church.valid?).to be true
+    end
+
+    it "should save" do
+      expect(church.save).to be true
+    end
+  end
 end
