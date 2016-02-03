410dccf40d78b41da19c32435d969d75344335fe
mikelangston <mikemac8203@gmail.com>
2016-02-02 21:17:31 -0500

    added postmark for mail delivery and added confirmable to devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203000223) do
+ActiveRecord::Schema.define(version: 20160203011120) do
 
   create_table "churches", force: :cascade do |t|
     t.string   "name",         limit: 255
@@ -46,8 +46,12 @@ ActiveRecord::Schema.define(version: 20160203000223) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "confirmation_token",     limit: 255
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
