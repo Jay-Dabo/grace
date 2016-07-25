41a63a8d4d4288fa9c012386f93edb847432b4a5
mikelangston <mikemac8203@gmail.com>
2016-07-25 12:52:49 -0400

    update to churches_controller_spec.rb

--- app/controllers/churches_controller.rb
+++ app/controllers/churches_controller.rb
@@ -13,6 +13,7 @@ class ChurchesController < ApplicationController
   # GET /churches/1
   # GET /churches/1.json
   def show
+    redirect_to churches_url
   end
 
   # GET /churches/new
--- spec/controllers/churches_controller_spec.rb
+++ spec/controllers/churches_controller_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe ChurchesController, type: :controller do
       sign_in user
     end
 
+    #test the index action
     describe "GET index" do
       it "has a 200 response status" do
         get :index
@@ -20,11 +21,26 @@ RSpec.describe ChurchesController, type: :controller do
       end
     end
 
+    #test the show action
+    describe "GET show" do
+      let(:church){ FactoryGirl.create(:church) }
+      it "redirects to index action" do
+        get :show, id: church
+        expect(response).to redirect_to(action: :index)
+      end
+    end
+
+    #text the edit action
     describe "GET edit" do
       let(:church){ FactoryGirl.create(:church) }
       it "has a 200 response status" do
         get :edit, id: church
         expect(response.status).to eq(200)
+      end
+
+      it "renders the edit template" do
+        get :edit, id: church
+        expect(response).to render_template(:edit)
       end
     end
 
