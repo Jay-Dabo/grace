88f5acf8c068d5ba4566dd6e8c4a9ab99a7a1fc1
mikelangston <mike@mikelangston.me>
2017-10-10 17:37:57 -0400

    update to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_church
 
   def after_sign_in_path_for(resource)
     current_church = current_user.church.id
@@ -11,4 +12,11 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     render file: "#{Rails.root}/public/403.html", status: 403, layout: "application"
   end
+
+  private
+    def set_church
+      if current_user
+        @church = current_user.church
+      end
+    end
 end
